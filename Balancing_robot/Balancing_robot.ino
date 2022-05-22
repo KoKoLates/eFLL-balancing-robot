d82cf23e2e28a2e11d51d9164176840f604a872f
KoKoLates <the21515@gmail.com>
2022-05-23 02:37:52 +0800

    update

--- Balancing_robot/Balancing_robot.ino
+++ Balancing_robot/Balancing_robot.ino
@@ -1,3 +1,8 @@
+#include <Wire.h>
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+#include <I2Cdev.h>
+
 #include <Fuzzy.h>
 
 # define RMotor_DIG 4
@@ -6,6 +11,9 @@
 # define LMotor_PWM 7
 
 Fuzzy *fuzzy = new Fuzzy();
+MPU6050 mpu;
+
+int16_t acc_y, acc_z, gyro_x;
 
 void fuzzy_set(){
   
@@ -18,9 +26,22 @@ void setup(){
   pinMode(LMotor_DIG, OUTPUT);
   pinMode(LMotor_PWM, OUTPUT);
 
-  fuzzy_set();
+  mpu.initialize();
+  mpu.setYAccelOffset(175);
+  mpu.setZAccelOffset(701);
+  mpu.setXGyroOffset(97);
 }
 
 void loop(){
-  
+  acc_y = mpu.getAccelerationY();
+  acc_z = mpu.getAccelerationZ();
+  gyro_x = mpu.getRotationX();
+  Serial.print(acc_y);
+  Serial.print(" ");
+  Serial.print(acc_z);
+  Serial.print(" ");
+  Serial.println(gyro_x);
+  float accAngle = atan2(acc_y, acc_z)*RAD_TO_DEG;
+  Serial.println(accAngle);
+  delay(1000);
 }
