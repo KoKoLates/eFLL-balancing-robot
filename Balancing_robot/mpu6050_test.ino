f9b6d085abfa0a38d6f8e424dc56bd861531cd31
KoKoLates <the21515@gmail.com>
2022-05-23 22:36:04 +0800

    update

--- Balancing_robot/mpu6050_test.ino
+++ Balancing_robot/mpu6050_test.ino
@@ -4,6 +4,49 @@
 #include <MPU6050.h>
 
 MPU6050 mpu;
+int16_t AccY, AccZ, GyroX;
+float AccAngle, GyroAngle;
+float yn=0, yn1=0, xn1=0;
+float a=0, a1=0, b1=0;
+float elapsedTime, currentTime, previousTime;
+
+void setup() {
+  Serial.begin(9600);
+  mpu.initialize();
+  mpu.setYAccelOffset(175);
+  mpu.setZAccelOffset(701);
+  mpu.setXGyroOffset(97);
+}
+
+void loop() {
+  //read accelerometer data//
+  AccY = mpu.getAccelerationY();
+  AccZ = mpu.getAccelerationZ();
+  //calculate the angle//
+  AccAngle = atan2(AccY, AccZ)*RAD_TO_DEG;
+  //low pass filter for accAngle
+  yn = 0.92*yn1 + 0.04*AccAngle + 0.04*xn1;
+  xn1=AccAngle;yn1=yn;
+  
+  //
+  //read gyroscope data//
+  previousTime = currentTime;
+  currentTime = millis();
+  elapsedTime = (currentTime - previousTime) / 1000;
+  GyroX = mpu.getRotationX()/131;
+  float gyroRate = GyroX;
+  a = 0.95*a1 + 0.025*gyroRate + 0.025*b1;
+  b1=gyroRate;a1=a;
+  GyroAngle += gyroRate*elapsedTime;
+  float angle = -0.01*GyroAngle + 1.01*yn;
+  Serial.print(a);
+  Serial.print(",");Serial.println(angle);
+}#include <Wire.h>
+#include <I2Cdev.h>
+#include <helper_3dmath.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
 int16_t acc_y, acc_z, gyro_x;
 long sampling_timer;
 float gyroAngle, currentangle;
