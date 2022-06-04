42b5124d4d7478879cb7123783d361dcf062ca4f
KoKoLates <the21515@gmail.com>
2022-06-05 00:00:29 +0800

    update

--- Filters/kalman_filter/kalman_filter.c
+++ Filters/kalman_filter/kalman_filter.c
@@ -6,6 +6,8 @@ void kalman1_init(kalman1 *state, float init_x, float init_p)
     state -> p = init_p;
     state -> a = 1;
     state -> h = 1;
+    state -> q = 0;
+    state -> r = 0;
 }
 
 void kalman2_init(kalman2 *state, float *init_x, float (*init_p)[2])
@@ -29,3 +31,16 @@ void kalman2_init(kalman2 *state, float *init_x, float (*init_p)[2])
     state -> q[1] = 0;
     state -> r = 0;
 }
+
+float kalman1_filter(kalman1 *state, float measure)
+{
+    state -> x = state -> a * state -> x;
+    state -> p = state -> a * state -> a * state -> p + state ->q;
+
+    state ->gain = state -> p * state -> h / (state -> p * state -> h * state -> h + state -> r);
+    state -> x = state -> x + state ->gain * (measure - state -> h * state -> x);
+    state -> p = (1 - state -> gain * state -> h) * state -> p;
+
+    return state -> x;
+}
+
