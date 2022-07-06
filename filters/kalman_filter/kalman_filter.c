6a6240f610747b508331504b266f38b070464471
KoKoLates <the21515@gmail.com>
2022-07-06 15:15:04 +0800

    add notations

--- filters/kalman_filter/kalman_filter.c
+++ filters/kalman_filter/kalman_filter.c
@@ -1,16 +1,19 @@
 # include "kalman_filter.h"
 
-void kalman1_init(kalman1 *state, float init_x, float init_p)
+// a, h, q, r is changable according to the application
+// Input parameter: state -> Kalman filter structure, init_x -> initial state, init_p -> initial estimated error
+
+void kalman1_init(Kalman_1D *state, float init_x, float init_p)
 {
     state -> x = init_x;
     state -> p = init_p;
     state -> a = 1;
     state -> h = 1;
-    state -> q = 0;
-    state -> r = 0;
+    state -> q = 10e-6; // predict noise convariance
+    state -> r = 10e-5; // measure error convariance
 }
 
-void kalman2_init(kalman2 *state, float *init_x, float (*init_p)[2])
+void kalman2_init(kalman_2D *state, float *init_x, float (*init_p)[2])
 {
     state -> x[0] = init_x[0];
     state -> x[1] = init_x[1];
@@ -19,52 +22,64 @@ void kalman2_init(kalman2 *state, float *init_x, float (*init_p)[2])
     state -> p[1][0] = init_p[1][0];
     state -> p[1][1] = init_p[1][1];
 
+    // a = [[1, 0.1], [0, 1]]
     state -> a[0][0] = 1;
-    state -> a[0][1] = 1;
-    state -> a[1][0] = 1;
+    state -> a[0][1] = 0.1;
+    state -> a[1][0] = 0;
     state -> a[1][1] = 1;
 
+    // h = [1, 0]
     state -> h[0] = 1;
     state -> h[1] = 0;
 
-    state -> q[0] = 0;
-    state -> q[1] = 0;
-    state -> r = 0;
+    state -> q[0] = 10e-7;
+    state -> q[1] = 10e-7;
+    state -> r = 10e-7;
 }
 
-float kalman1_filter(kalman1 *state, float measure)
+float kalman1_filter(Kalman_1D *state, float measure)
 {
+    // Prediction
     state -> x = state -> a * state -> x;
-    state -> p = state -> a * state -> a * state -> p + state ->q;
+    state -> p = state -> a * state -> a * state -> p + state ->q; // p(n|n-1) = a * a * p(n-1|n-1) + q
 
-    state ->gain = state -> p * state -> h / (state -> p * state -> h * state -> h + state -> r);
+    // Measurement
+    state -> gain = state -> p * state -> h / (state -> p * state -> h * state -> h + state -> r);
     state -> x = state -> x + state ->gain * (measure - state -> h * state -> x);
     state -> p = (1 - state -> gain * state -> h) * state -> p;
 
-    return state -> x;
+    return state -> x; // Estimated result
 }
 
-float kalman2_filter(kalman2 *state, float measure)
+float kalman2_filter(kalman_2D *state, float measure)
 {
     float temp0 = 0.0f, temp1 = 0.0f, temp2 = 0.0f;
+
+    // Prediction
     state -> x[0] = state -> a[0][0] * state -> x[0] + state -> a[0][1] * state -> x[1];
     state -> x[1] = state -> a[1][0] * state -> x[0] + state -> a[1][1] * state -> x[1];
 
+    // p(n|n-1) = a * a * p(n-1|n-1) + q
     state -> p[0][0] = state -> a[0][0] * state -> p[0][0] + state -> a[0][1] * state -> p[1][0] + state -> q[0];
     state -> p[0][1] = state -> a[0][0] * state -> p[0][1] + state -> a[1][1] * state -> p[1][1];
     state -> p[1][0] = state -> a[1][0] * state -> p[0][0] + state -> a[0][1] * state -> p[1][0];
     state -> p[1][1] = state -> a[1][0] * state -> p[0][1] + state -> a[1][1] * state -> p[1][1] + state -> q[1];
 
+    // Measurement
+    // gain = p * h[]^T / [r + h * p * h[]^T]
     temp1 = state -> p[0][0] * state -> h[0] + state -> p[0][1] * state -> h[1];
     temp2 = state -> p[1][0] * state -> h[0] + state -> p[1][1] * state -> h[1];
     temp0 = state -> r + state -> h[0] * temp0 + state -> h[1] * temp1;
     state -> gain[0] = temp1 / temp2;
     state -> gain[1] = temp2 / temp0;
     
+    // X(n|n) = x(n|n-1) + gain(n) * [measure - h(n) * x(n|n-1)]
     temp0 = state -> h[0] * state -> x[0] + state -> h[1] * state -> x[1];
     state -> x[0] = state -> x[0] + state -> gain[0] * (measure - temp1); 
     state -> x[1] = state -> x[1] + state -> gain[1] * (measure - temp2);
 
+    // Update
+    // p(n|n) = [I - gain * h] * p(n|n-1)
     state -> p[0][0] = (1 - state -> gain[0] * state -> h[0]) * state -> p[0][0];
     state -> p[0][1] = (1 - state -> gain[0] * state -> h[1]) * state -> p[0][1];
     state -> p[1][0] = (1 - state -> gain[1] * state -> h[0]) * state -> p[1][0];
--- filters/kalman_filter/kalman_filter.h
+++ filters/kalman_filter/kalman_filter.h
@@ -1,21 +1,40 @@
 # ifndef _KALMAN_FILTER_H__
 # define _KALMAN_FILTER_H__
 
+// The measurement only for one dimension
+
+// Struct defination for one dimension Kalman filter
 typedef struct{
-    float x, a, h;
-    float q, r, p;
+    float x; // state
+    // x(n) = a * x(n-1) + u(n) : u(n) ~ N(0,q)
+    // z(n) = h * x(n) + w(n) : w(n) ~ N(0,r)
+    float a, q;
+    float h, r;
+    // q -> Predict(process) noise convariance
+    // r -> Measure noise convariance
+    float p; // Estimated error convariance
     float gain;
-}kalman1;
+}Kalman_1D;
 
+// Struct defination for two dimension Kalman filter
 typedef struct{
-    float x[2], a[2][2], h[2];
-    float q[2], p[2][2], r;
+    float x[2]; // state: angle and angle rate
+    // x(n) = a * x(n-1) + u(n) : u(n) ~ N(0,q)
+    // z(n) = h * x(n) + w(n) : w(n) ~ N(0,r)
+    float a[2][2], q[2];
+    float h[2], r;
+    // q -> predict(process) noise convariance
+    // r -> measure noise convariance
+    float p[2][2]; // Estimated error convariance [[p0, 01], [p2, p3]]
     float gain[2];
-}kalman2;
+}kalman_2D;
 
-extern void kalman1_init(kalman1 *state, float init_x, float init_p);
-extern void kalman2_init(kalman2 *state, float *init_x, float (*init_p)[2]);
-extern float kalman1_filter(kalman1 *state, float measure);
-extern float kalman2_filter(kalman2 *state, float measure);
+// Two type dimension state Kalman filter initialize
+extern void kalman1_init(Kalman_1D *state, float init_x, float init_p);
+extern void kalman2_init(kalman_2D *state, float *init_x, float (*init_p)[2]);
 
-# endif
+// Update and Measurement
+extern float kalman1_filter(Kalman_1D *state, float measure);
+extern float kalman2_filter(kalman_2D *state, float measure);
+
+# endif // _KALMAN_FILTER_H__
